4f0c215ac1070c8de2c63b01a5ab45debaaa3cfb
gomeshud <gomeshud@users.noreply.github.com>
2018-12-09 19:21:21 -0300

    If(A==B)  B=10;  imprimir(B);'  B = var3

--- 2. Linguagem de Expressoes II/ast.py
+++ 2. Linguagem de Expressoes II/ast.py
@@ -56,5 +56,5 @@ class If (Construcao):
     def __init__(self, var1, var2, var3, var4):
         if var1 == var2
            var3 = var4
-           print(var2)
+           print(var3)
         elif
