cdbe4157a7cc27378be745b9ddb2bd0d4d87d519
gomeshud <gomeshud@users.noreply.github.com>
2018-12-09 21:00:29 -0300

    def interpretar e self var = var

--- 2. Linguagem de Expressoes II/ast.py
+++ 2. Linguagem de Expressoes II/ast.py
@@ -54,10 +54,12 @@ class Imprimir (Construcao):
 
 class Se (Construcao):
     def __init__(self, var1, var2, var3, var4):
-        self.var1
-        self.var2
-        self.var3
-        self.var4
+        self.var1 = var1
+        self.var2 = var2
+        self.var3 = var3
+        self.var4 = var4
+
+    def interpretar(self):
         if var1 == var2
            var3 = var4
            return Imprimir
