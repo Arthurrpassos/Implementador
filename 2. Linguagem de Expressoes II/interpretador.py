be6afd9f1c1af24a33d7e381cdfb4b4270930bd6
gomeshud <gomeshud@users.noreply.github.com>
2018-12-14 00:35:57 -0300

    -tokens 9

--- 2. Linguagem de Expressoes II/interpretador.py
+++ 2. Linguagem de Expressoes II/interpretador.py
@@ -49,7 +49,6 @@ for construcao in programa:
                 tokens[inicio + 4][0],
                 tokens[inicio + 6][0],
                 tokens[inicio + 8][0],
-                tokens[inicio + 9][0],
         )
         se.interpretar()
 
